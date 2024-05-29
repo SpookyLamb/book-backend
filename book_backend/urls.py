e144514b191e0e64c1ac6d60e13283d99de667d6
SpookyLamb <turtleytyrant@gmail.com>
2024-05-29 09:53:08 -0400

    User setup.

--- book_backend/urls.py
+++ book_backend/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for book_backend project.
+URL configuration for project_auth project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from catalog_app.views import *
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('profile/', get_profile),
+    path('token/', TokenObtainPairView.as_view()),
+    path('create-user/', create_user),
 ]
